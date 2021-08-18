406ae3af2a13103c225df31a8d4761a2ac2f1cd8
Salikram-Mate <89122898+Salikram-Mate@users.noreply.github.com>
2021-08-18 13:29:32 +0530

    changed by salikram-mate Account

--- src/main/scala/com/Scala/project/gitrepo.scala
+++ src/main/scala/com/Scala/project/gitrepo.scala
@@ -8,6 +8,7 @@ object gitrepo {
       val data = spark.read.option("header","true").csv("C:\\Users\\Aai\\OneDrive\\Desktop\\study\\spark-code-scala\\Emptable.csv")
       data.show()
       println("changes from the git") //changed by git owner
+      println("changes from stream2learn") //changed by salikram-mate Account
   }
 
 }
