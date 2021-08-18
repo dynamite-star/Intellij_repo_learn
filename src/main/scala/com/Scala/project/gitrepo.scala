36a1efb092083f94038970dfcb7968c32c47ebed
Sairaj-Mate <sai.mate@rediffmail.com>
2021-08-18 11:55:48 +0530

    Initial commit

---
+++ src/main/scala/com/Scala/project/gitrepo.scala
@@ -0,0 +1,12 @@
+package com.Scala.project
+
+import org.apache.spark.sql.SparkSession
+
+object gitrepo {
+    def main(args:Array[String]): Unit ={
+      val spark = SparkSession.builder().appName("sairajApp").master("local").getOrCreate()
+      val data = spark.read.option("header","true").csv("C:\\Users\\Aai\\OneDrive\\Desktop\\study\\spark-code-scala\\Emptable.csv")
+      data.show()
+  }
+
+}
